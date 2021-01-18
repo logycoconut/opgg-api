924a72d0ca98ff12e4a9ca4f2d51ab2b1025a4ec
logycoconut <1425795337@qq.com>
2021-01-18 11:37:08 +0800

    增加一些注释

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # opgg-api
+
+> 基于 OP.GG 的 LOL 数据爬取，是 stephenpoole / op.gg-api 的 Java 版本，可以独立部署成后端服务，通过接口访问
+> 
+> 算是 LOL 资深爱好者的一点小贡献吧 (*^_^*)
+
+
+## 参考链接
+
+[stephenpoole / op.gg-api](https://github.com/stephenpoole/op.gg-api)
+[jsoup官方文档](https://jsoup.org/cookbook/)
+[JSoup快速入门](https://www.yiibai.com/jsoup/jsoup-quick-start.html)
--- src/main/java/com/logycoconut/opgg/api/parser/Endpoint.java
+++ src/main/java/com/logycoconut/opgg/api/parser/Endpoint.java
@@ -2,15 +2,27 @@ package com.logycoconut.opgg.api.parser;
 
 /**
  * @author hall
- * @description
+ * @description 网页解析接口
  * @date 2021-01-11 22:45
  */
 public interface Endpoint {
 
+    /**
+     * 定义数据路径
+     * @return 数据所在网页地址
+     */
     String path();
 
+    /**
+     * 解析方法
+     * @return 解析后的数据
+     */
     String parse();
 
+    /**
+     * 通用请求方法
+     * @return dom文档
+     */
     String request();
 
 }
