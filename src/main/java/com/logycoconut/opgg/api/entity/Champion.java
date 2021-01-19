52af1c9614d0f97fc837ff0db2cfa6691708551c
logycoconut <1425795337@qq.com>
2021-01-19 17:50:40 +0800

    改造Champion类以及ChampionRole类

--- src/main/java/com/logycoconut/opgg/api/entity/Champion.java
+++ src/main/java/com/logycoconut/opgg/api/entity/Champion.java
@@ -4,15 +4,13 @@ import lombok.Data;
 
 /**
  * @author hall
- * @description 英雄类
+ * @description 英雄基本信息
  * @date 2021-01-13 21:42
  */
 @Data
 public class Champion {
 
-    private Long id;
     private String nameZh;
     private String nameEn;
-    private String imageUrl;
 
 }
--- src/main/java/com/logycoconut/opgg/api/entity/ChampionRole.java
+++ src/main/java/com/logycoconut/opgg/api/entity/ChampionRole.java
@@ -2,19 +2,20 @@ package com.logycoconut.opgg.api.entity;
 
 import com.logycoconut.opgg.api.enums.Role;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author hall
- * @description 英雄与角色对应关系
+ * @description 英雄角色信息
  * @date 2021-01-13 22:18
  */
 @Data
-public class ChampionRole {
+@EqualsAndHashCode(callSuper = true)
+public class ChampionRole extends Champion{
 
-    private Long id;
-    private String championNameEn;
     private Role role;
-    private String level;
-    private String rank;
+    private String tier;
+    private String winRate;
+    private String pickRate;
 
 }
