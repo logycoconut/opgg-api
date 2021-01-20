66fcd98af8ab20c984b5e2d1c944b222dc26ac9b
logycoconut <1425795337@qq.com>
2021-01-20 17:51:21 +0800

    加入测试类

---
+++ src/test/java/com/logycoconut/opgg/api/parser/ChampionEndpointTest.java
@@ -0,0 +1,24 @@
+package com.logycoconut.opgg.api.parser;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author hall
+ * @date 2021/1/20
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class ChampionEndpointTest {
+
+    @Autowired
+    private ChampionEndpoint championEndpoint;
+
+    @Test
+    void parse() {
+//        championEndpoint.parse()
+    }
+}
