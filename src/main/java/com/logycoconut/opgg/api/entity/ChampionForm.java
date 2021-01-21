76571e502cf71245cb7df72dd5c634d5eec17469
logycoconut <1425795337@qq.com>
2021-01-21 18:03:08 +0800

    增加英雄表单对象和符文对象

---
+++ src/main/java/com/logycoconut/opgg/api/entity/ChampionForm.java
@@ -0,0 +1,16 @@
+package com.logycoconut.opgg.api.entity;
+
+import lombok.Data;
+
+/**
+ * @author hall
+ * @description 英雄参数
+ * @date 2021/1/21
+ */
+@Data
+public class ChampionForm {
+
+    private String nameEn;
+    private String role;
+
+}
---
+++ src/main/java/com/logycoconut/opgg/api/entity/Rune.java
@@ -0,0 +1,17 @@
+package com.logycoconut.opgg.api.entity;
+
+import lombok.Data;
+
+/**
+ * @author hall
+ * @description 天赋（主系、副系、小符文）
+ * @date 2021/1/21
+ */
+@Data
+public class Rune {
+
+    private String winRate;
+    private String pickRate;
+
+
+}
